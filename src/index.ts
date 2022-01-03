7d136f3c833ed4adb6cf9c90d6bfc892f15403de
Maksym Menzheha <masimfyd@gmail.com>
2022-01-03 20:51:59 +0200

    initial server setup

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,37 @@
 import { MikroORM } from "@mikro-orm/core";
+import { ApolloServer } from "apollo-server-express";
+import express from "express";
 import { __prod__ } from "./constants";
-import { Post } from "./entities/Post";
+//import { Post } from "./entities/Post";
 import mikroOrmConfig from "./mikro-orm.config";
+import { buildSchema } from 'type-graphql';
+import { HelloResolver } from "./resolvers/hello";
 
-const main = async() => {
+const main = async () => {
+
     const orm = await MikroORM.init(mikroOrmConfig);
-    orm.getSchemaGenerator().updateSchema();
-    // const post = orm.em.create(Post, {title: 'my first post'});
-    // await orm.em.persistAndFlush(post);
+    await orm.getSchemaGenerator().updateSchema();
 
-    const posts = await orm.em.find(Post, {});
-    console.log(posts);
+    const app = express();
+
+    const apolloServer = new ApolloServer({
+        schema: await buildSchema({
+            resolvers: [
+                HelloResolver
+            ],
+            validate: false
+        })
+    });
+
+    await apolloServer.start();
+    apolloServer.applyMiddleware({ app });
+
+    app.listen(4000, () => {
+        console.log('server started on localhost 4000');
+    })
 }
 
 //
 main().catch(err => {
     console.error(err);
-}) ;
+});
