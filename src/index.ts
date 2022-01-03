c02133096e5b5091e81036a8c91136f6f6803a16
Maksym Menzheha <masimfyd@gmail.com>
2022-01-03 23:52:43 +0200

    user register resolver

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
 import "reflect-metadata";
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
-//import { Post } from "./entities/Post";
 import mikroOrmConfig from "./mikro-orm.config";
 import express from "express";
 import { ApolloServer } from "apollo-server-express";
 import { buildSchema } from 'type-graphql';
 import { HelloResolver } from "./resolvers/hello";
 import { PostResolver } from "./resolvers/post";
+import { UserResolver } from "./resolvers/user";
 
 const main = async () => {
 
@@ -20,7 +20,8 @@ const main = async () => {
         schema: await buildSchema({
             resolvers: [
                 HelloResolver,
-                PostResolver
+                PostResolver,
+                UserResolver
             ],
             validate: false
         }),
