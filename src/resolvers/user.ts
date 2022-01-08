fda1eae336bc9958978ea4003e64d92d3ce02e65
Maksym Menzheha <masimfyd@gmail.com>
2022-01-08 20:56:02 +0200

    Logout

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -3,6 +3,7 @@ import { MyContext } from "src/types";
 import { Arg, Ctx, Field, InputType, Mutation, ObjectType, Query, Resolver } from "type-graphql";
 import argon2 from "argon2";
 import { EntityManager } from "@mikro-orm/postgresql";
+import { COOKIE_NAME } from "../constants";
 
 @InputType()
 class UsernamePasswordInput {
@@ -133,5 +134,23 @@ export class UserResolver {
         return {
             user,
         };
+    }
+
+    @Mutation(() => Boolean)
+    logout(
+        @Ctx() { req, res }: MyContext
+    ) {
+        return new Promise(resolve =>
+            req.session.destroy((err) => {
+                res.clearCookie(COOKIE_NAME);
+                if (err) {
+                    console.log(err);
+                    resolve(false)
+                    return
+                }
+
+                resolve(true)
+            })
+        );
     }
 }
