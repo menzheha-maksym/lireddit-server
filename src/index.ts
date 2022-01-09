a51028ec1520b9533b9516f6f7aea4bd40cc3dd9
Maksym Menzheha <masimfyd@gmail.com>
2022-01-09 21:25:50 +0200

    start change password

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ import { buildSchema } from 'type-graphql';
 import { HelloResolver } from "./resolvers/hello";
 import { PostResolver } from "./resolvers/post";
 import { UserResolver } from "./resolvers/user";
-import redis from 'redis';
+import Redis from 'ioredis';
 import session from 'express-session';
 import connectRedis from 'connect-redis';
 import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
@@ -22,7 +22,7 @@ const main = async () => {
 
 
     const RedisStore = connectRedis(session);
-    const redisClient = redis.createClient();
+    const redis = new Redis();
 
     app.use(cors({
         origin: "http://localhost:3000",
@@ -33,7 +33,7 @@ const main = async () => {
         session({
             name: COOKIE_NAME,
             store: new RedisStore({
-                client: redisClient,
+                client: redis,
                 disableTouch: true,
             }),
             cookie: {
@@ -57,7 +57,7 @@ const main = async () => {
             ],
             validate: false,
         }),
-        context: ({ req, res }) => ({ em: orm.em, req, res }),
+        context: ({ req, res }) => ({ em: orm.em, req, res, redis }),
         plugins: [
             ApolloServerPluginLandingPageGraphQLPlayground(),
         ]
