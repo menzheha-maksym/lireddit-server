c88e1562a2740c814bfbe0aa88284f1fb1421f03
Maksym Menzheha <masimfyd@gmail.com>
2022-01-10 23:33:47 +0200

    Many to One

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -1,5 +1,15 @@
 import { Post } from "../entities/Post";
-import { Arg, Mutation, Query, Resolver } from "type-graphql";
+import { Arg, Ctx, Field, InputType, Mutation, Query, Resolver, UseMiddleware } from "type-graphql";
+import { MyContext } from "src/types";
+import { isAuth } from "../middleware/isAuth";
+
+@InputType()
+class PostInput {
+    @Field()
+    title: string
+    @Field()
+    text: string
+}
 
 @Resolver()
 export class PostResolver {
@@ -16,10 +26,15 @@ export class PostResolver {
     }
 
     @Mutation(() => Post)
+    @UseMiddleware(isAuth)
     async createPost(
-        @Arg("title") title: string,
+        @Arg("input") input: PostInput,
+        @Ctx() { req }: MyContext
     ): Promise<Post> {
-        return Post.create({ title }).save();
+        return Post.create({
+            ...input,
+            creatorId: req.session.userId,
+        }).save();
     }
 
     @Mutation(() => Post, { nullable: true })
@@ -32,7 +47,7 @@ export class PostResolver {
             return null;
         }
         if (typeof title !== 'undefined') {
-            await Post.update({id}, {title});
+            await Post.update({ id }, { title });
         }
         return post;
     }
