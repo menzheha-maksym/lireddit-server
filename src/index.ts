f95aa501209a11d8930212173f339e4a7315c95a
Maksym Menzheha <masimfyd@gmail.com>
2022-01-11 17:23:19 +0200

    Adding Mock Data

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,7 @@ import cors from 'cors';
 import { createConnection } from 'typeorm';
 import { Post } from "./entities/Post";
 import { User } from "./entities/User";
+import path from "path";
 
 const main = async () => {
     const conn = await createConnection({
@@ -23,11 +24,14 @@ const main = async () => {
         password: 'admin',
         logging: true,
         synchronize: true,
+        migrations: [path.join(__dirname, "./migrations/*")],
         entities: [
             Post, 
             User,
         ]
     });
+
+    await conn.runMigrations()
 
     const app = express();
 
