53547024b26d0d5797691927392a6d1fd17273ef
Maksym Menzheha <masimfyd@gmail.com>
2022-01-30 01:49:09 +0200

    Edit Post

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -169,18 +169,26 @@ export class PostResolver {
     }
 
     @Mutation(() => Post, { nullable: true })
+    @UseMiddleware(isAuth)
     async updatePost(
-        @Arg("id") id: number,
+        @Arg("id", () => Int) id: number,
         @Arg("title", () => String, { nullable: true }) title: string,
+        @Arg("text") text: string,
+        @Ctx() { req }: MyContext,
     ): Promise<Post | null> {
-        const post = await Post.findOne(id);
-        if (!post) {
-            return null;
-        }
-        if (typeof title !== 'undefined') {
-            await Post.update({ id }, { title });
-        }
-        return post;
+        const result = await getConnection()
+            .createQueryBuilder()
+            .update(Post)
+            .set({ title, text })
+            .where('id = :id and "creatorId" = :creatorId', {
+                id,
+                creatorId: req.session.userId
+            })
+            .returning("*")
+            .execute();
+
+        return result.raw[0];
+
     }
 
     @Mutation(() => Boolean)
