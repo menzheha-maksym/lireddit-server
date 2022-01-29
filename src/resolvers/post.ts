938ce2a1004ec4173c344eede025cfe970de9497
Maksym Menzheha <masimfyd@gmail.com>
2022-01-30 00:41:45 +0200

    Delete Post

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -184,10 +184,25 @@ export class PostResolver {
     }
 
     @Mutation(() => Boolean)
+    @UseMiddleware(isAuth)
     async deletePost(
-        @Arg("id") id: number,
+        @Arg("id", () => Int) id: number,
+        @Ctx() { req }: MyContext
     ): Promise<boolean> {
-        await Post.delete(id);
+        // not cascade way
+        // const post = await Post.findOne(id);
+        // if (!post) {
+        //     return false;
+        // }
+
+        // if (post.creatorId !== req.session.userId) {
+        //     throw new Error('not authorized');
+        // }
+
+        // await Updoot.delete({ postId: id });
+        // await Post.delete({ id });
+
+        await Post.delete({ id, creatorId: req.session.userId });
         return true;
     }
 }
